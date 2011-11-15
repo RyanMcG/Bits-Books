2d4774eea9dc168d98509093ef217987be3bb654
Ryan McGowan <ryan@ryanmcg.com>
2011-11-15 14:51:17 -0500

    Added some routes to web.py.

--- web.py
+++ web.py
@@ -1,10 +1,64 @@
 import os
-from flask import Flask
+from flask import Flask, send_from_directory
 app = Flask(__name__)
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+            'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 @app.route('/')
-def hello_world():
+def main():
+    #Show main page with search
     return 'Hello World!'
+
+@app.route('/search')
+def search():
+    pass
+
+@app.route('/user/view/history')
+def add_book_to_cart():
+    pass
+
+@app.route('/user/view/order/jsjsakjsak')
+def add_book_to_cart():
+    pass
+
+@app.route('/user/view/cart')
+def add_book_to_cart():
+    pass
+
+@app.route('/user/view/profile')
+def add_book_to_cart():
+    pass
+
+@app.route('/user/edit/profile')
+def add_book_to_cart():
+    pass
+
+@app.route('/user/add/book/skjdnaskdsak')
+def add_book_to_cart():
+    pass
+
+@app.route('/admin/create/book')
+def create_book():
+    pass
+
+@app.route('/admin/view/inventory/jdjdsjdskjds')
+def create_book():
+    pass
+
+@app.route('/admin/view/inventory_order/jdjdsjdskjds')
+def create_book():
+    pass
+
+@app.route('/admin/view/')
+def create_book():
+    pass
+
+@app.route('/admin/create/inventory_order')
+def create_book():
+    pass
 
 if __name__ == '__main__':
     #Change this to false when on production
