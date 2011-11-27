985046d22f679ad82db6678f5641cb6c34045828
Ryan McGowan <ryan@ryanmcg.com>
2011-11-27 17:10:44 -0500

    Added constraint specification to finalize.sql.

--- finalize.sql
+++ finalize.sql
@@ -3,62 +3,62 @@
 
 -- Step 3: Setup Foreign Keys
 -- Book
-ALTER TABLE Book ADD FOREIGN KEY(publisher_id) REFERENCES Publisher(id);
+ALTER TABLE Book ADD CONSTRAINT FOREIGN KEY(publisher_id) REFERENCES Publisher(id);
 
 -- Admin
-ALTER TABLE Admin ADD FOREIGN KEY(user_id) REFERENCES User(id);
+ALTER TABLE Admin ADD CONSTRAINT FOREIGN KEY(user_id) REFERENCES User(id);
 
 -- Author
 
 -- Billing
-ALTER TABLE Billing ADD FOREIGN KEY(user_id) REFERENCES User(id);
+ALTER TABLE Billing ADD CONSTRAINT FOREIGN KEY(user_id) REFERENCES User(id);
 
 -- Book_Author
-ALTER TABLE Book_Author ADD FOREIGN KEY(book_id) REFERENCES Book(id);
-ALTER TABLE Book_Author ADD FOREIGN KEY(author_id) REFERENCES Author(id);
+ALTER TABLE Book_Author ADD CONSTRAINT FOREIGN KEY(book_id) REFERENCES Book(id);
+ALTER TABLE Book_Author ADD CONSTRAINT FOREIGN KEY(author_id) REFERENCES Author(id);
 
 -- Book_Category
-ALTER TABLE Book_Category ADD FOREIGN KEY(category_id) REFERENCES Category(id);
-ALTER TABLE Book_Category ADD FOREIGN KEY(book_id) REFERENCES Book(id);
+ALTER TABLE Book_Category ADD CONSTRAINT FOREIGN KEY(category_id) REFERENCES Category(id);
+ALTER TABLE Book_Category ADD CONSTRAINT FOREIGN KEY(book_id) REFERENCES Book(id);
 
 -- Cart
-ALTER TABLE Cart ADD FOREIGN KEY(order_id) REFERENCES `Order`(id);
+ALTER TABLE Cart ADD CONSTRAINT FOREIGN KEY(order_id) REFERENCES `Order`(id);
 
 -- CartItem
-ALTER TABLE CartItem ADD FOREIGN KEY(cart_id) REFERENCES Cart(id);
-ALTER TABLE CartItem ADD FOREIGN KEY(book_id) REFERENCES Book(id);
+ALTER TABLE CartItem ADD CONSTRAINT FOREIGN KEY(cart_id) REFERENCES Cart(id);
+ALTER TABLE CartItem ADD CONSTRAINT FOREIGN KEY(book_id) REFERENCES Book(id);
 
 -- Category
 
 -- Inventory
-ALTER TABLE Inventory ADD FOREIGN KEY(book_id) REFERENCES Book(id);
+ALTER TABLE Inventory ADD CONSTRAINT FOREIGN KEY(book_id) REFERENCES Book(id);
 
 -- InventoryOrder
-ALTER TABLE InventoryOrder ADD FOREIGN KEY(inventory_id) REFERENCES Inventory(id);
+ALTER TABLE InventoryOrder ADD CONSTRAINT FOREIGN KEY(inventory_id) REFERENCES Inventory(id);
 
 -- Order
-ALTER TABLE `Order` ADD FOREIGN KEY(cart_id) REFERENCES Cart(id);
-ALTER TABLE `Order` ADD FOREIGN KEY(user_id) REFERENCES User(id);
+ALTER TABLE `Order` ADD CONSTRAINT FOREIGN KEY(cart_id) REFERENCES Cart(id);
+ALTER TABLE `Order` ADD CONSTRAINT FOREIGN KEY(user_id) REFERENCES User(id);
 
 -- OrderItem
-ALTER TABLE OrderItem ADD FOREIGN KEY(book_id) REFERENCES Book(id);
-ALTER TABLE OrderItem ADD FOREIGN KEY(order_id) REFERENCES `Order`(id);
+ALTER TABLE OrderItem ADD CONSTRAINT FOREIGN KEY(book_id) REFERENCES Book(id);
+ALTER TABLE OrderItem ADD CONSTRAINT FOREIGN KEY(order_id) REFERENCES `Order`(id);
 
 -- OrderPayment
-ALTER TABLE OrderPayment ADD FOREIGN KEY(order_id) REFERENCES `Order`(id);
-ALTER TABLE OrderPayment ADD FOREIGN KEY(billing_id) REFERENCES Billing(id);
+ALTER TABLE OrderPayment ADD CONSTRAINT FOREIGN KEY(order_id) REFERENCES `Order`(id);
+ALTER TABLE OrderPayment ADD CONSTRAINT FOREIGN KEY(billing_id) REFERENCES Billing(id);
 
 -- Publisher
 
 -- User
 
 -- UserAddress
-ALTER TABLE UserAddress ADD FOREIGN KEY(user_id) REFERENCES `User`(id);
+ALTER TABLE UserAddress ADD CONSTRAINT FOREIGN KEY(user_id) REFERENCES `User`(id);
 
 -- Creditcard
-ALTER TABLE Creditcard ADD FOREIGN KEY(address_id) REFERENCES `UserAddress`(id);
-ALTER TABLE Creditcard ADD FOREIGN KEY(billing_id) REFERENCES Billing(id);
+ALTER TABLE Creditcard ADD CONSTRAINT FOREIGN KEY(address_id) REFERENCES `UserAddress`(id);
+ALTER TABLE Creditcard ADD CONSTRAINT FOREIGN KEY(billing_id) REFERENCES Billing(id);
 
 -- Giftcard
-ALTER TABLE GirftCard ADD FOREIGN KEY(billing_id) REFERENCES Billing(id);
+ALTER TABLE Giftcard ADD CONSTRAINT FOREIGN KEY(billing_id) REFERENCES Billing(id);
 
